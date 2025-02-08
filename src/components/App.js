4a008e10b2073d6f3d2ac63a46143d8848b90b0a
MMKT1977 <kennedy.moranga@student.moringaschool..com>
2025-02-09 01:36:12 +0300

    header and About components added

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,20 @@ import React from "react";
 import blogData from "../data/blog";
 import Header from "./Header";
 import About from "./About";
+import ArticleList from "./ArticleList";
 
 console.log(blogData);
 
-const { name ,image, about}= blogData;
+const { name ,image, about, posts}= blogData;
 
 function App() {
   return (
     <div className="App">
       
-      <Header name = {name}/>
+      <Header name ={name}/>
       <About image ={image} about={about}/>
+      <ArticleList posts={post}/>
+
     </div>
   );
 }
