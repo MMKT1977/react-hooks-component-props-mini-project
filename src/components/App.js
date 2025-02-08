590d8c7c8df440b143d34075e828365e06a2c236
MMKT1977 <kennedy.moranga@student.moringaschool..com>
2025-02-09 01:19:24 +0300

    Header and About component added

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header";
+import About from "./About";
 
 console.log(blogData);
+
+const { name ,image, about}= blogData;
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      
+      <Header name = {name}/>
+      <About image ={image} about={about}/>
     </div>
   );
 }
